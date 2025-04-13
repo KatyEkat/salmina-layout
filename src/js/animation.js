eab4222b707d7d108d3ce146200c8029ae7ea079
Katy <katy@Katys.local>
2025-04-13 19:59:46 +0300

    feat(site): added html + styles + js logic

---
+++ src/js/animation.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+	window.addEventListener("scroll", () => {
+		const header = document.querySelector(".header");
+		if (window.scrollY > 0) {
+			header.classList.add("sticky");
+		} else {
+			header.classList.remove("sticky");
+		}
+	});
+
+	const submitToggle = document.getElementById("submitToggle");
+	const sendFormBtn = document.getElementById("sendFormBtn");
+	const error = document.getElementById("error");
+	const success = document.getElementById("success");
+	const form = document.getElementById("myForm");
+
+	let isSubmitActive = false;
+
+	submitToggle.addEventListener("click", () => {
+		isSubmitActive = !isSubmitActive;
+		submitToggle.classList.toggle("active");
+	});
+
+	sendFormBtn.addEventListener("click", (e) => {
+		e.preventDefault();
+
+		if (!isSubmitActive) {
+			error.classList.add("show");
+			setTimeout(() => {
+				error.classList.remove("show");
+			}, 3000);
+			return;
+		}
+
+		success.classList.add("show");
+		setTimeout(() => {
+			success.classList.remove("show");
+			form.reset();
+			submitToggle.classList.remove("active");
+			isSubmitActive = false;
+		}, 3000);
+	});
+});
